8dc9d2f32fa4fb7a4b841c9779086a9b6f6e147b
kimsh0306 <b666790@gmail.com>
2024-08-23 23:10:44 +0900

    my-json-server

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -11,8 +11,8 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get("q") || "";
     console.log("searchQuery: ", searchQuery)
-    let url = `http://localhost:4000/products?q=${searchQuery}`;
-    // let url = `https://my-json-server.typicode.com/<kimsh0306>/<noona-project-hnm>/products?q=${searchQuery}`;
+    // let url = `http://localhost:4000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/<kimsh0306>/<noona-project-hnm>/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -10,8 +10,8 @@ const ProductDetail = () => {
   let { id } = useParams();
 
   const getProductDetail = async () => {
-    let url = `http://localhost:4000/products/${id}`;
-    // let url = `https://my-json-server.typicode.com/<kimsh0306>/<noona-project-hnm>/products/${id}`;
+    // let url = `http://localhost:4000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/<kimsh0306>/<noona-project-hnm>/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
